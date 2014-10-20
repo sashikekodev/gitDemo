92725a7b7b659756465829d6f5021d44a5760a0a
sashikeodev <sashikeodev@gamil.com>
2014-10-20 07:44:59 -0600

    service: value and constant testing

--- WebContent/js/controllers/MessageController.js
+++ WebContent/js/controllers/MessageController.js
@@ -1,12 +1,21 @@
 /**
  * 
  */
-angular.module("GitApp")
-	.controller("MessageController", ["$scope", function($scope)
+var app = angular.module("GitApp")
+	.controller("MessageController", ["$scope", "messageValueService", "myVal", "myVal2", function($scope, messageValueService, myVal, myVal2)
 	{
+		console.log(messageValueService, myVal(), myVal2);
+		
+		myVal2.text ="changed";
+		console.log(myVal2);
 		$scope.val = false;
 		$scope.hide = function()
 		{
 			$scope.val = !$scope.val;
 		};
-	}]);
+	}])
+	.value("myVal", function(){return "myVal";})
+	.value("myVal2", {text: "myVal2"});
+	
+
+	app.constant("messageValueService", {msg: "This is the message value service"});
--- WebContent/js/projectScripts.js
+++ WebContent/js/projectScripts.js
@@ -12,3 +12,4 @@ angular.module("GitApp", ["ngRoute"])
 			.when("/login", 	{templateUrl: 'templates/login.html'})
 			.otherwise({redirectTo: '/home'});
 	});
+
